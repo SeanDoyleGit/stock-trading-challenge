d595c56fc11f1dbf735c15b2c45c029af557537a
sean doyle <gigafud@gmail.com>
2019-04-06 12:19:42 +1100

    set up server to server react app

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = process.env.PORT || 5000;
 
-//Route setup
-app.get('/', (req, res) => {
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'client/build')));
   
-  res.send('root route');
-})
-//Start server
+    app.get('*', (req, res) => {
+        res.sendfile(path.join(__dirname = 'client/build/index.html'));
+    });
+}
+
 app.listen(port, (req, res) => {
-    console.log(`server listening on port: ${port}`)
+    console.log( `server listening on port: ${port}`);
 });
