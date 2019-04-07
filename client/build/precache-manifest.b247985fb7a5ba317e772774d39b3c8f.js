774afa9ea0ccb22667f37dfba04e114e53d3ceb7
sean doyle <gigafud@gmail.com>
2019-04-08 07:21:36 +1000

    prepare to deploy

---
+++ client/build/precache-manifest.b247985fb7a5ba317e772774d39b3c8f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ffe67f5d67374c7ecba9",
+    "url": "/static/js/main.95ba9bb3.chunk.js"
+  },
+  {
+    "revision": "b83bfbff75224693f3d6",
+    "url": "/static/js/2.fd96a112.chunk.js"
+  },
+  {
+    "revision": "ffe67f5d67374c7ecba9",
+    "url": "/static/css/main.2de25f17.chunk.css"
+  },
+  {
+    "revision": "58e7b84e30a4233976dc0ea9b4476577",
+    "url": "/index.html"
+  }
+];
